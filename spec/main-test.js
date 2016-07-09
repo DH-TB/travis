abce35b654119803146bb6b962252bb20621a226
DH-TB <929659475@qq.com>
2016-07-09 14:32:35 +0800

    travis

---
+++ spec/main-test.js
@@ -0,0 +1,12 @@
+var add = require('../src/main.js');
+describe("travis", function () {
+
+    var input1 = 1;
+    var input2 = 2;
+
+    it("should add two number", function () {
+        var expectText = 3;
+
+        expect(add(input1, input2)).toEqual(expectText);
+    })
+});
---
+++ src/main.js
@@ -0,0 +1,5 @@
+function add(input1,input2) {
+    return input1+input2;
+}
+
+module.exports = add;
