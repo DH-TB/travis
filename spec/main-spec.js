4cd73cd8b8623d59396875b14ad281d429aba468
DH-TB <929659475@qq.com>
2016-07-20 16:01:26 +0800

    a

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -655,10 +655,8 @@ describe('unit test', function () {
 买二赠一商品：
 名称：雪碧，数量：1瓶                
 名称：荔枝，数量：0斤`;
-
             expect(mainData.promotionsText(receiptItems)).toEqual(expectText);
         });
     });
 
 });
-
--- src/main.js
+++ src/main.js
@@ -87,7 +87,8 @@ function promotionsText(receiptItems) {
 
         if (receiptItem.promotionType == 'BUY_TWO_GET_ONE_FREE') {
             title = (receiptItem.promotionType) ? ('----------------------\n买二赠一商品：') : '';
-            text += '\n'+`名称：${cartItem.item.name}，数量：${receiptItem.saveCount}${cartItem.item.unit}`;
+            text += `
+                名称：${cartItem.item.name}，数量：${receiptItem.saveCount}${cartItem.item.unit}`;
         }
     }
     text = title + text;
@@ -113,8 +114,6 @@ ${promotionsText(receipt.receiptItems) || ''}
 节省：${formate(receipt.allSaved)}(元)
 **********************`;
 }
-
-
 
 let formate = (price)=> {
     return price.toFixed(2);
